787b3de1fb0fcd2cf6c68b27ae9fa6b220b9e80f
suyogmungale <suyogmungale5@gmail.com>
2023-04-10 22:21:00 +0530

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import logo1 from './logo1.png';
+
+export {
+  download,
+  logo,
+  preview,
+  logo1
+};
